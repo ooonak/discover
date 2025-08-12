7b59088b40267fef5298b132380e7dfd07581828
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2025-08-12 22:27:53 +0200

    chore: Refactored from dynamic (trait object) to static dispatch (generic type)

--- src/application/discover_service.rs
+++ src/application/discover_service.rs
@@ -2,17 +2,17 @@ use std::slice;
 
 use crate::domain::{Device, DeviceListener, ViewUpdates};
 
-pub struct DiscoverService<'a> {
-    ui: &'a dyn ViewUpdates,
+pub struct DiscoverService<'a, T: ViewUpdates + 'a> {
+    ui: &'a T,
 }
 
-impl<'a> DiscoverService<'a> {
-    pub fn new(ui: &'a dyn ViewUpdates) -> Self {
+impl<'a, T: ViewUpdates + 'a> DiscoverService<'a, T> {
+    pub fn new(ui: &'a T) -> Self {
         Self { ui }
     }
 }
 
-impl<'a> DeviceListener for DiscoverService<'a> {
+impl<'a, T: ViewUpdates + 'a> DeviceListener for DiscoverService<'a, T> {
     fn on_device_discovered(&self, device: Device) {
         
         println!("DiscoverService (application), new device from driving port (DeviceListener), forwarding to driven port (Viewupdates): {:?}", device);
